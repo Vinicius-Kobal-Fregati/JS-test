8db50b0257090b13d64949c26bdab823245d5dcd
Vinicius-Kobal-Fregati <61463329+Vinicius-Kobal-Fregati@users.noreply.github.com>
2020-05-05 09:36:53 -0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+let myImage = document.querySelector ('img');
+
+myImage.onclick = function () {mySrc = myImage.getAttribute ('src');
+    if (mySrc ==='images/capa.jpg'){
+        myImage.setAttribute ('src', 'images/kyojuro.jpg');
+    } else {
+        myImage.setAttribute ('src', 'images/capa.jpg');
+    }
+}
+// por que precisa deste let ?
+// Isso faz com que ao tocar na imagem da capa, ela altera para outra imagem, clicando pela segunda vez, volta a primeira.
+let #botão = document.querySelector('#botão');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Capa do filme, ' + myName;
+  }
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Capa do filme, ' + storedName;
+  }
+  myButton.onclick = function() {
+    setUserName();
+  }
+
+  
